42ce17d9d493756b4f6ba18b2d928e98e7f94a5f
elenamoron <93elena@hotmail.es>
2017-06-18 22:23:31 +0200

    modificaciones

--- src/Backtracking.java
+++ src/Backtracking.java
@@ -19,8 +19,8 @@ public class Backtracking {
     public Regalo[] actualizarRegalos(Regalo regalo, Regalo[] regalos){
         for(int i=0; i< regalos.length; i++){
             if(regalo.getNumero() == regalos[i].getNumero()){
-                regalos[i].setPeso(regalo.getPeso());
-                regalos[i].setAlegria(regalo.getAlegria());
+                regalos[i].setPeso(0);
+                regalos[i].setAlegria(0);
             }
         }
         return regalos;
@@ -37,77 +37,115 @@ public class Backtracking {
         }
         return beneficio;
     }
-    
+    public Alfombra mejorSolucion(Alfombra alfombra, int[][] soluciones, Regalo[] regalos){
+        int alegria = 0;
+        int solucion = 0;
+        for(int i=0;i<soluciones.length;i++){
+            for (int j=0;j<soluciones.length;j++){
+                if(j==0){
+                    alfombra.setRegalo(regalos[soluciones[solucion][j]], j, regalos[soluciones[solucion][j]].getNumero());
+                } else if (soluciones[i][j]!=0){
+                    alfombra.setRegalo(regalos[soluciones[solucion][j]], j, regalos[soluciones[solucion][j]].getNumero());
+                }
+            }
+            if(alegria < alfombra.sumarAlegria(alfombra.getRegalos())){
+                alegria = alfombra.sumarAlegria(alfombra.getRegalos());
+                solucion = i;
+                alfombra.setRegalos(new Regalo[regalos.length]);
+                alfombra.setRegalosIntro(0);
+                
+            }
+        }
+        alfombra.setRegalos(new Regalo[regalos.length]);
+        alfombra.setRegalosIntro(0);
+        for (int j=0;j<soluciones.length;j++){
+            if(j==0){
+               alfombra.setRegalo(regalos[soluciones[solucion][j]], j, regalos[soluciones[solucion][j]].getNumero());
+               alfombra.setRegalosIntro(alfombra.getRegalosIntro());
+            } else if (soluciones[solucion][j]!=0){
+                alfombra.setRegalo(regalos[soluciones[solucion][j]], j, regalos[soluciones[solucion][j]].getNumero());
+                alfombra.setRegalosIntro(alfombra.getRegalosIntro());
+            }
+        }
+        return alfombra;
+    }
     public Alfombra algoritmo(Regalo [] regalos, Alfombra alfombra,int nivel, int mejorBeneficio, int[] solucion, int pesoAlfombra, Regalo[] regaloAux, File fis) throws IOException{
         int i=numSolucion;
-        
-        while(alfombra.getPeso()>0 && i< regalos.length){
-           if(nivel == 0 || nivel != i){
-                if(regalos[i].getPeso()<=alfombra.getPeso() && regalos[i].getPeso()!=0){
-                    if(alfombra.getRegalosIntro()!= 0){
-                        if(alfombra.getRegalos()[alfombra.getRegalosIntro()-1].getNumero() != regalos[i].getNumero()){
+        while(numSolucion < regalos.length){
+            while(alfombra.getPeso()>0 && i< regalos.length){
+               if(nivel == 0 || nivel != i){
+                    if(regalos[i].getPeso()<=alfombra.getPeso() && regalos[i].getPeso()!=0){
+                        if(alfombra.getRegalosIntro()!= 0){
+                            if(alfombra.getRegalos()[alfombra.getRegalosIntro()-1]!=null && alfombra.getRegalos()[alfombra.getRegalosIntro()-1].getNumero() != regalos[i].getNumero()){
+                                alfombra.setRegalo(regalos[i], alfombra.getRegalosIntro(),regalos[i].getNumero());
+                                alfombra.setPeso(alfombra.getPeso()-regalos[i].getPeso());
+                                alfombra.setRegalosIntro(alfombra.getRegalosIntro());
+                            }
+                        }else{
                             alfombra.setRegalo(regalos[i], alfombra.getRegalosIntro(),regalos[i].getNumero());
                             alfombra.setPeso(alfombra.getPeso()-regalos[i].getPeso());
                             alfombra.setRegalosIntro(alfombra.getRegalosIntro());
                         }
-                    }else{
-                        alfombra.setRegalo(regalos[i], alfombra.getRegalosIntro(),regalos[i].getNumero());
-                        alfombra.setPeso(alfombra.getPeso()-regalos[i].getPeso());
-                        alfombra.setRegalosIntro(alfombra.getRegalosIntro());
                     }
-                }
-                i++;
-           }else{
-               if(regalos[i+1].getPeso()<=alfombra.getPeso() && regalos[i+1].getPeso()!=0){
-                     alfombra.setRegalo(regalos[i+1], alfombra.getRegalosIntro(), regalos[i+1].getNumero());
-                     alfombra.setPeso(alfombra.getPeso()-regalos[i+1].getPeso());
-                     alfombra.setRegalosIntro(alfombra.getRegalosIntro());
-                 }
-                i++;
-           }
-        }
-        
-        for(int j=0; j<alfombra.getRegalos().length;j++){
-            if(alfombra.getRegalos()[j]!=null){
-                solucion[j] = alfombra.getRegalos()[j].getNumero(); 
+                    i++;
+               }else{
+                   if(regalos[i+1].getPeso()<=alfombra.getPeso() && regalos[i+1].getPeso()!=0){
+                         alfombra.setRegalo(regalos[i+1], alfombra.getRegalosIntro(), regalos[i+1].getNumero());
+                         alfombra.setPeso(alfombra.getPeso()-regalos[i+1].getPeso());
+                         alfombra.setRegalosIntro(alfombra.getRegalosIntro());
+                     }
+                    i++;
+               }
             }
-        }
-        
-        int beneficioEncontrado = beneficio(solucion,regalos);
-        if(beneficioEncontrado>mejorBeneficio){
-            soluciones[numSolucion]= solucion;
-            mejorBeneficio = beneficioEncontrado;
-        }
-        
-        nivel = alfombra.getRegalosIntro();
-        if(alfombra.getRegalosIntro()-1 >0 ){
-            regalos[alfombra.getRegalos()[alfombra.getRegalosIntro()-1].getNumero()].setAlegria(0);
-            regalos[alfombra.getRegalos()[alfombra.getRegalosIntro()-1].getNumero()].setPeso(0);
-        }
-        while(solucionesPorRegalo < regalos.length){
-            alfombra.setPeso(pesoAlfombra);
+
+            for(int j=0; j<alfombra.getRegalos().length;j++){
+                if(alfombra.getRegalos()[j]!=null){
+                    solucion[j] = alfombra.getRegalos()[j].getNumero(); 
+                }
+            }
+
+            int beneficioEncontrado = beneficio(solucion,regalos);
+            if(beneficioEncontrado>mejorBeneficio){
+                soluciones[numSolucion]= solucion;
+                mejorBeneficio = beneficioEncontrado;
+            }
+
+            nivel = alfombra.getRegalosIntro();
+            if(alfombra.getRegalosIntro()-1 >0 ){
+                regalos[alfombra.getRegalos()[alfombra.getRegalosIntro()-1].getNumero()].setAlegria(0);
+                regalos[alfombra.getRegalos()[alfombra.getRegalosIntro()-1].getNumero()].setPeso(0);
+            }
+            while(solucionesPorRegalo < regalos.length){
+                alfombra.setPeso(pesoAlfombra);
+                alfombra.setRegalos(new Regalo[regalos.length]);
+                solucionesPorRegalo++;    
+                alfombra.setRegalosIntro(0);
+                if(nivel!=0){
+                    nivel--;
+                }
+                solucion = new int[regalos.length];
+                algoritmo(regalos, alfombra,nivel,mejorBeneficio,solucion,pesoAlfombra,regaloAux,fis);
+
+
+            }
+            numSolucion++;
+            solucionesPorRegalo=0;
             alfombra.setRegalos(new Regalo[regalos.length]);
-            solucionesPorRegalo++;    
+            alfombra.setPeso(pesoAlfombra);
             alfombra.setRegalosIntro(0);
+            if(alfombrasllenas == 0){
+                regaloAux = m.leerFichero(fis);
+            }
             if(nivel!=0){
                 nivel--;
             }
-            algoritmo(regalos, alfombra,nivel,mejorBeneficio,solucion,pesoAlfombra,regaloAux,fis);
-            
-            
+            algoritmo(regaloAux, alfombra,nivel,mejorBeneficio,solucion,pesoAlfombra,regaloAux,fis);
         }
-        numSolucion++;
-        solucionesPorRegalo=0;
-        alfombra.setRegalos(new Regalo[regalos.length]);
-        alfombra.setPeso(pesoAlfombra);
-        alfombra.setRegalosIntro(0);
-        regaloAux = m.leerFichero(fis);
-        if(nivel!=0){
-            nivel--;
+        if(alfombrasllenas == 0){
+            regaloAux = m.leerFichero(fis);
         }
-        algoritmo(regaloAux, alfombra,nivel,mejorBeneficio,solucion,pesoAlfombra,regaloAux,fis);
-    
-        
+        alfombra = mejorSolucion(alfombra,soluciones,regaloAux);
+        alfombra.setPeso(0);
         return alfombra;
     }
     public Alfombra[] RepartirRegalos(Regalo[] regalos, Alfombra A, Alfombra B, Alfombra C, File fichero) throws IOException{
@@ -126,6 +164,13 @@ public class Backtracking {
             int [] solucion = new int[regalos.length];
             alfombraMayor = algoritmo(regalos,alfombraMayor,nivel,mejorBeneficio,solucion, pesoAlfombra,regalosAux,fichero);
             alfombrasllenas++;
+            if(alfombrasllenas<1){
+                regalos = m.leerFichero(fichero);
+            }
+            for(int i=0; i< alfombraMayor.getRegalosIntro();i++){
+                regalos = actualizarRegalos(alfombraMayor.getRegalos()[i],regalos);
+            }
+            
         }
         
         return alfombras;
